917d2d73cb761615cdd7ed710ee76fe988067b05
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-19 21:12:55 +0800

    ttt completed!

--- works/ttt/ttt.js
+++ works/ttt/ttt.js
@@ -2,14 +2,14 @@
 
 const grids = document.querySelectorAll('.grid')
 const title = document.querySelector('h2')
+const resetBtn = document.querySelector('.reset-btn')
+const displayWinner = document.querySelector('.winner')
 const r1c1 = document.querySelector('.r1c1')
 const r1c2 = document.querySelector('.r1c2')
 const r1c3 = document.querySelector('.r1c3')
-
 const r2c1 = document.querySelector('.r2c1')
 const r2c2 = document.querySelector('.r2c2')
 const r2c3 = document.querySelector('.r2c3')
-
 const r3c1 = document.querySelector('.r3c1')
 const r3c2 = document.querySelector('.r3c2')
 const r3c3 = document.querySelector('.r3c3')
@@ -18,114 +18,23 @@ let nextPlayer,currentPlayer, pattern, toggle = true, firstPlayer = 1, gameActiv
 
 title.innerHTML = `Player ${firstPlayer} please.`
 
-const addPattern = (target) =>{
-    if(toggle){
-        currentPlayer = 1
-        nextPlayer = 2
-        pattern = 'O'
-        toggle = !toggle
-    } else {
-        currentPlayer = 2
-        nextPlayer = 1
-        pattern = 'X'
-        toggle = !toggle
-    }
-    target.innerHTML = pattern
-    title.innerHTML = `Player ${nextPlayer} please.`
-    // console.log(r1c1.innerHTML)
-    isWon()
-console.log(gameState)
 
+const wonMessage = () =>{
+    title.innerHTML = `Player ${currentPlayer} won!`
 }
 
-// const shownextPlayer = () =>{
-//     title.innerHTML = `Player ${toggle ? 1 : 2}`
-// }
-
-for(let i of grids){
-    i.addEventListener('click', (e)=>{
-        if(!isClick(e.target.innerHTML) && gameActive){
-            addPattern(i)
-        } else {
-            return
-        }
-        
+const resetGame = () =>{
+    grids.forEach(grid=>{
+        grid.innerHTML = null
     })
+    gameActive = true
+    nextPlayer = null
+    firstPlayer = 1
+    currentPlayer = firstPlayer
+    title.innerHTML = `Player ${firstPlayer} please.`
 }
 
-const counting = () =>{
-    let row1 = 0
-    let row2 = 0
-    let row3 = 0
-    let col1 = 0
-    let col2 = 0
-    let col3 = 0
-    let leftcross = 0
-    let rightcross = 0
-    
-}
-
-const gameState = [[r1c1.innerHTML, r1c2.innerHTML, r1c3.innerHTML], [r2c1.innerHTML, r2c2.innerHTML, r2c3.innerHTML], [r3c1.innerHTML, r3c2.innerHTML, r3c3.innerHTML]]
-
-console.log(gameState[0])
-
-const isWon = () =>{
-    // if(gameState[0].every(isO) || gameState[0].every(isX) ){    }
-    if(r1c1.innerHTML === 'O' && r1c2.innerHTML === 'O' && r1c3.innerHTML === 'O' ){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c1.innerHTML === 'X' && r1c2.innerHTML === 'X' && r1c3.innerHTML === 'X' ){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r2c1.innerHTML === 'O' && r2c2.innerHTML === 'O' && r2c3.innerHTML === 'O'){
-        console.log('123')
-        gameActive = false
-        return true
-    
-    } else if(r2c1.innerHTML === 'X' && r2c2.innerHTML === 'X' && r2c3.innerHTML === 'X'){
-        console.log('123')
-        gameActive = false
-        return true
-    
-    } else if(r3c1.innerHTML === 'O' && r3c2.innerHTML === 'O'&& r3c3.innerHTML === 'O'){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r3c1.innerHTML === 'X' && r3c2.innerHTML === 'X'&& r3c3.innerHTML === 'X'){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c1.innerHTML === 'O' && r2c1.innerHTML === 'O' && r3c1.innerHTML === 'O'){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c1.innerHTML === 'X' && r2c1.innerHTML === 'X' && r3c1.innerHTML === 'X'){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c2.innerHTML === r2c2.innerHTML && r2c2.innerHTML === r3c2.innerHTML){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c3.innerHTML === r2c3.innerHTML && r2c3.innerHTML === r3c3.innerHTML){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c1.innerHTML === r2c2.innerHTML && r2c2.innerHTML === r3c3.innerHTML){
-        console.log('123')
-        gameActive = false
-        return true
-    } else if(r1c3.innerHTML === r2c2.innerHTML && r2c2.innerHTML === r3c1.innerHTML){
-        console.log('123')
-        gameActive = false
-        return true
-    }
-}
-
-// const isO = str => str === 'O'
-// const isX = str => str === 'X'
+resetBtn.addEventListener('click', resetGame)
 
 const isClick = (target) =>{
     if(target){
@@ -134,3 +43,89 @@ const isClick = (target) =>{
         return false
     }
 }
+
+const addPattern = (target) =>{
+        if(toggle){
+            currentPlayer = 1
+            nextPlayer = 2
+            pattern = 'O'
+            toggle = !toggle
+        } else {
+            currentPlayer = 2
+            nextPlayer = 1
+            pattern = 'X'
+            toggle = !toggle
+        }
+    target.innerHTML = pattern
+    title.innerHTML = `Player ${nextPlayer} please.`
+}
+
+for(let i of grids){
+    i.addEventListener('click', (e)=>{
+        if(!isClick(e.target.innerHTML) && gameActive){
+            addPattern(i)
+            setTimeout(()=>{
+                if(isWon()){
+                    gameActive = false
+                    wonMessage()
+                }
+            },0)
+        } else {
+            return
+        }
+    })
+}
+
+const isWon = () =>{
+    if(r1c1.innerHTML === 'O' && r1c2.innerHTML === 'O' && r1c3.innerHTML === 'O' ){
+        console.log('123')
+        return true
+    } else if(r1c1.innerHTML === 'X' && r1c2.innerHTML === 'X' && r1c3.innerHTML === 'X' ){
+        console.log('123')
+        return true
+    } else if(r2c1.innerHTML === 'O' && r2c2.innerHTML === 'O' && r2c3.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r2c1.innerHTML === 'X' && r2c2.innerHTML === 'X' && r2c3.innerHTML === 'X'){
+        console.log('123')
+        return true
+    } else if(r3c1.innerHTML === 'O' && r3c2.innerHTML === 'O'&& r3c3.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r3c1.innerHTML === 'X' && r3c2.innerHTML === 'X'&& r3c3.innerHTML === 'X'){
+        console.log('123')
+        return true
+    } else if(r1c1.innerHTML === 'O' && r2c1.innerHTML === 'O' && r3c1.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r1c1.innerHTML === 'X' && r2c1.innerHTML === 'X' && r3c1.innerHTML === 'X'){
+        console.log('123')
+        return true
+    } else if(r1c2.innerHTML === 'O' && r2c2.innerHTML === 'O' && r3c2.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r1c2.innerHTML === 'X' && r2c2.innerHTML === 'X' && r3c2.innerHTML === 'X'){
+        console.log('123')
+        return true
+    } else if(r1c3.innerHTML === 'O' && r2c3.innerHTML === 'O' && r3c3.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r1c3.innerHTML === 'X' && r2c3.innerHTML === 'X' && r3c3.innerHTML === 'X'){
+        console.log('123')
+        return true
+    }else if(r1c1.innerHTML === 'O' && r2c2.innerHTML === 'O' && r3c3.innerHTML === 'O'){
+        console.log('123')
+        return true
+    } else if(r1c1.innerHTML === 'X' && r2c2.innerHTML === 'X' && r3c3.innerHTML === 'X'){
+        console.log('123')
+        return true
+    } else if(r1c3.innerHTML === 'O' && r2c2.innerHTML === 'O' && r3c1.innerHTML=== 'O'){
+        console.log('123')
+        return true
+    }  else if(r1c3.innerHTML === 'X' && r2c2.innerHTML === 'X' && r3c1.innerHTML=== 'X'){
+        console.log('123')
+        return true
+    }
+}
+
+document.addEventListener('DOMContentLoaded', isWon)
