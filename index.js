3d50adcd7c55718bef548e02d177c1e49e7c6809
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-19 22:03:19 +0800

    change big-img responsive

--- index.js
+++ index.js
@@ -5,81 +5,36 @@ const navItem = document.querySelectorAll('.nav-item')
 const smallImg = document.querySelectorAll('.small-img')
 const bigImg = document.querySelector('.big-img')
 
-const imgArr = [
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    },
-    {
-        id: '',
-        src: '',
-        alt: ''
-    }
-]
-
-
-toggleBtn.addEventListener('click', ()=>{
+toggleBtn.addEventListener('click', () => {
     toggleNav()
 })
 
-const toggleNav = () =>{
+const toggleNav = () => {
     let toggle = toggleList.style
-    toggle.left = toggle.left === '0px' ? toggle.left = '-40%' : toggle.left ='0px' ;
+    toggle.left = toggle.left === '0px' ? toggle.left = '-40%' : toggle.left = '0px';
 }
 
-const changeImg = (targetImg) =>{
+const changeImg = (targetImg) => {
     bigImg.src = targetImg.src
     bigImg.id = targetImg.id
     bigImg.alt = targetImg.alt
-
 }
 
-smallImg.forEach(img=>{
-    img.addEventListener('click', ({target})=>{
+smallImg.forEach(img => {
+    img.addEventListener('click', ({ target }) => {
         changeImg(target)
     })
 })
 
-navItem.forEach(item =>{
-    item.addEventListener('click', ()=>{
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
         toggleNav()
     })
 })
 
 // advice.addEventListener("mouseover", e=>{
 //     e.target.innerHTML = 'Click to give me adivces'
-    
+
 // })
 // advice.addEventListener("mouseout",e=>{
 //     e.target.innerHTML = 'Give Me Advices'
@@ -90,9 +45,25 @@ navItem.forEach(item =>{
 //     getUserAdvices()
 // })
 
-const getUserAdvices= () =>{
+const getUserAdvices = () => {
     let userInput = window.prompt('Thank you for your advices?')
-    if(userInput){
+    if (userInput) {
         window.alert(`Your advice:${userInput}`)
     }
 }
+
+const showWeather = async () => {
+    let apiKey = '4976d245ae88b90b3d05b2808b36fb24'
+    try {
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=hongkong&appid=${apiKey}`)
+        if (response.ok) {
+            let result = await response.json()
+            console.log(result)
+        }
+    } catch (err) {
+            console.log(err.message)
+    }
+
+    
+}
+showWeather()
