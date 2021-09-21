0b8082bc9a8e643681a531e4ecae48ee4c2b9cc9
skylar2021 <maplewong5@yahoo.com.hk>
2021-09-21 11:40:50 +0800

    add name to photos

--- index.js
+++ index.js
@@ -1,57 +1,11 @@
+"use strict"
+
 const advice = document.querySelector('.advices')
 const toggleBtn = document.querySelector('.nav-icon')
 const toggleList = document.querySelector('nav.web-nav')
-const navItem = document.querySelectorAll('.nav-item')
 const smallImg = document.querySelectorAll('.small-img')
-const bigImg = document.querySelector('.big-img')
 
-toggleBtn.addEventListener('click', () => {
-    toggleNav()
-})
-
-const toggleNav = () => {
-    let toggle = toggleList.style
-    toggle.left = toggle.left === '0px' ? toggle.left = '-40%' : toggle.left = '0px';
-}
-
-const changeImg = (targetImg) => {
-    bigImg.src = targetImg.src
-    bigImg.id = targetImg.id
-    bigImg.alt = targetImg.alt
-}
-
-smallImg.forEach(img => {
-    img.addEventListener('click', ({ target }) => {
-        changeImg(target)
-    })
-})
-
-navItem.forEach(item => {
-    item.addEventListener('click', () => {
-        toggleNav()
-    })
-})
-
-// advice.addEventListener("mouseover", e=>{
-//     e.target.innerHTML = 'Click to give me adivces'
-
-// })
-// advice.addEventListener("mouseout",e=>{
-//     e.target.innerHTML = 'Give Me Advices'
-// })
-
-// advice.addEventListener('click', ()=>{
-//     // let userInput = window.prompt('Thank you for your advices?')
-//     getUserAdvices()
-// })
-
-const getUserAdvices = () => {
-    let userInput = window.prompt('Thank you for your advices?')
-    if (userInput) {
-        window.alert(`Your advice:${userInput}`)
-    }
-}
-
+// show today weather 
 const showWeather = async () => {
     let apiKey = '4976d245ae88b90b3d05b2808b36fb24'
     try {
@@ -80,3 +34,53 @@ const date = () =>{
 
 showWeather()
 date()
+
+// toggle navbar
+const toggleNav = () => {
+    let toggle = toggleList.style
+    toggle.left = toggle.left === '0px' ? toggle.left = '-40%' : toggle.left = '0px';
+}
+
+toggleBtn.addEventListener('click', () => {
+    toggleNav()
+})
+
+const navItem = document.querySelectorAll('.nav-item')
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        toggleNav()
+    })
+})
+
+const changeImg = (targetImg) => {
+    const bigImg = document.querySelector('.big-img')
+    bigImg.src = targetImg.src
+    bigImg.id = targetImg.id
+    bigImg.alt = targetImg.alt
+}
+
+smallImg.forEach(img => {
+    img.addEventListener('click', ({ target }) => {
+        changeImg(target)
+    })
+})
+
+// advice.addEventListener("mouseover", e=>{
+//     e.target.innerHTML = 'Click to give me adivces'
+
+// })
+// advice.addEventListener("mouseout",e=>{
+//     e.target.innerHTML = 'Give Me Advices'
+// })
+
+// advice.addEventListener('click', ()=>{
+//     // let userInput = window.prompt('Thank you for your advices?')
+//     getUserAdvices()
+// })
+
+// const getUserAdvices = () => {
+//     let userInput = window.prompt('Thank you for your advices?')
+//     if (userInput) {
+//         window.alert(`Your advice:${userInput}`)
+//     }
+// }
